be48710d04974073392344e71cbc2d73bb511a47
Michael Jackson <mike.jackson@bluequartz.net>
2019-07-11 14:05:50 -0400

    ITK4 and ITK5 Compile fixes
    * Clang-formatted code
    * replaced typedef with using where possible.
    * Added missing typename specifiers where needed.
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- DREAM3DReviewFilters/ITKResampleImage.h
+++ DREAM3DReviewFilters/ITKResampleImage.h
@@ -3,13 +3,15 @@
  */
 
 #pragma once
+
 #include <QtCore/QFile>
-#include "SIMPLib/SIMPLib.h"
+
 #include "SIMPLib/Common/SIMPLibSetGetMacros.h"
-#include "SIMPLib/Filtering/AbstractFilter.h"
-#include "SIMPLib/ITK/itkInPlaceDream3DDataToImageFilter.h"
-#include "SIMPLib/Geometry/TransformContainer.h"
 #include "SIMPLib/FilterParameters/FileListInfoFilterParameter.h"
+#include "SIMPLib/Filtering/AbstractFilter.h"
+#include "SIMPLib/Geometry/TransformContainer.h"
+#include "SIMPLib/ITK/itkInPlaceDream3DDataToImageFilter.h"
+#include "SIMPLib/SIMPLib.h"
 
 #include "DREAM3DReview/DREAM3DReviewFilters/util/ITKTransformHelpers.h"
 #include "DREAM3DReview/DREAM3DReviewPlugin.h"
@@ -20,188 +22,192 @@
 class DREAM3DReview_EXPORT ITKResampleImage : public AbstractFilter
 {
   Q_OBJECT
-    // clang-format off
+  // clang-format off
     PYB11_CREATE_BINDINGS(ITKResampleImage SUPERCLASS AbstractFilter)
     PYB11_PROPERTY(QString TransformFileName READ getTransformFileName WRITE setTransformFileName)
     PYB11_PROPERTY(int InterpolationType READ getInterpolationType WRITE setInterpolationType)
-	PYB11_PROPERTY(DataArrayPath MovingImageArrayPath READ getMovingImageArrayPath WRITE setMovingImageArrayPath)
-	PYB11_PROPERTY(DataArrayPath DataContainerName READ getDataContainerName WRITE setDataContainerName)
-	PYB11_PROPERTY(QString CellAttributeMatrixName READ getCellAttributeMatrixName WRITE setCellAttributeMatrixName)
-	PYB11_PROPERTY(QString ImageDataArrayName READ getImageDataArrayName WRITE setImageDataArrayName)
-	PYB11_PROPERTY(FileListInfo_t ImageFileListInfo READ getImageFileListInfo WRITE setImageFileListInfo)
-	PYB11_PROPERTY(FileListInfo_t OrientationFileListInfo READ getOrientationFileListInfo WRITE setOrientationFileListInfo)
-	PYB11_PROPERTY(QString OutputPath READ getOutputPath WRITE setOutputPath)
-	PYB11_PROPERTY(QString OutputPathEBSD READ getOutputPathEBSD WRITE setOutputPathEBSD)
-	PYB11_PROPERTY(QString FileNamePrefix READ getFileNamePrefix WRITE setFileNamePrefix)
-	PYB11_PROPERTY(QString EBSDFileNamePrefix READ getEBSDFileNamePrefix WRITE setEBSDFileNamePrefix)
+    PYB11_PROPERTY(DataArrayPath MovingImageArrayPath READ getMovingImageArrayPath WRITE setMovingImageArrayPath)
+    PYB11_PROPERTY(DataArrayPath DataContainerName READ getDataContainerName WRITE setDataContainerName)
+    PYB11_PROPERTY(QString CellAttributeMatrixName READ getCellAttributeMatrixName WRITE setCellAttributeMatrixName)
+    PYB11_PROPERTY(QString ImageDataArrayName READ getImageDataArrayName WRITE setImageDataArrayName)
+    PYB11_PROPERTY(FileListInfo_t ImageFileListInfo READ getImageFileListInfo WRITE setImageFileListInfo)
+    PYB11_PROPERTY(FileListInfo_t OrientationFileListInfo READ getOrientationFileListInfo WRITE setOrientationFileListInfo)
+    PYB11_PROPERTY(QString OutputPath READ getOutputPath WRITE setOutputPath)
+    PYB11_PROPERTY(QString OutputPathEBSD READ getOutputPathEBSD WRITE setOutputPathEBSD)
+    PYB11_PROPERTY(QString FileNamePrefix READ getFileNamePrefix WRITE setFileNamePrefix)
+    PYB11_PROPERTY(QString EBSDFileNamePrefix READ getEBSDFileNamePrefix WRITE setEBSDFileNamePrefix)
+  // clang-format on
 
-    // clang-format on
+public:
+  SIMPL_SHARED_POINTERS(ITKResampleImage)
+  SIMPL_FILTER_NEW_MACRO(ITKResampleImage)
+  SIMPL_TYPE_MACRO_SUPER(ITKResampleImage, AbstractFilter)
 
-  public:
-    SIMPL_SHARED_POINTERS(ITKResampleImage)
-    SIMPL_FILTER_NEW_MACRO(ITKResampleImage)
-    SIMPL_TYPE_MACRO_SUPER(ITKResampleImage, AbstractFilter)
+  ~ITKResampleImage() override;
 
-    ~ITKResampleImage() override;
+  SIMPL_FILTER_PARAMETER(QString, OutputPath)
+  Q_PROPERTY(QString OutputPath READ getOutputPath WRITE setOutputPath)
 
-	SIMPL_FILTER_PARAMETER(QString, OutputPath)
-	Q_PROPERTY(QString OutputPath READ getOutputPath WRITE setOutputPath)
+  SIMPL_FILTER_PARAMETER(QString, FileNamePrefix)
+  Q_PROPERTY(QString FileNamePrefix READ getFileNamePrefix WRITE setFileNamePrefix)
 
-	SIMPL_FILTER_PARAMETER(QString, FileNamePrefix)
-	Q_PROPERTY(QString FileNamePrefix READ getFileNamePrefix WRITE setFileNamePrefix)
+  SIMPL_FILTER_PARAMETER(QString, EBSDFileNamePrefix)
+  Q_PROPERTY(QString EBSDFileNamePrefix READ getEBSDFileNamePrefix WRITE setEBSDFileNamePrefix)
 
-	SIMPL_FILTER_PARAMETER(QString, EBSDFileNamePrefix)
-	Q_PROPERTY(QString EBSDFileNamePrefix READ getEBSDFileNamePrefix WRITE setEBSDFileNamePrefix)
+  SIMPL_FILTER_PARAMETER(QString, OutputPathEBSD)
+  Q_PROPERTY(QString OutputPathEBSD READ getOutputPathEBSD WRITE setOutputPathEBSD)
 
-	SIMPL_FILTER_PARAMETER(QString, OutputPathEBSD)
-	Q_PROPERTY(QString OutputPathEBSD READ getOutputPathEBSD WRITE setOutputPathEBSD)
+  SIMPL_FILTER_PARAMETER(QString, TransformFileName)
+  Q_PROPERTY(QString TransformFileName READ getTransformFileName WRITE setTransformFileName)
 
-    SIMPL_FILTER_PARAMETER(QString, TransformFileName)
-    Q_PROPERTY(QString TransformFileName READ getTransformFileName WRITE setTransformFileName)
+  SIMPL_FILTER_PARAMETER(int, InterpolationType)
+  Q_PROPERTY(int InterpolationType READ getInterpolationType WRITE setInterpolationType)
 
-    SIMPL_FILTER_PARAMETER(int, InterpolationType)
-    Q_PROPERTY(int InterpolationType READ getInterpolationType WRITE setInterpolationType)
+  SIMPL_FILTER_PARAMETER(DataArrayPath, MovingImageArrayPath)
+  Q_PROPERTY(DataArrayPath MovingImageArrayPath READ getMovingImageArrayPath WRITE setMovingImageArrayPath)
 
-	SIMPL_FILTER_PARAMETER(DataArrayPath, MovingImageArrayPath)
-	Q_PROPERTY(DataArrayPath MovingImageArrayPath READ getMovingImageArrayPath WRITE setMovingImageArrayPath)
+  SIMPL_FILTER_PARAMETER(DataArrayPath, DataContainerName)
+  Q_PROPERTY(DataArrayPath DataContainerName READ getDataContainerName WRITE setDataContainerName)
 
-	SIMPL_FILTER_PARAMETER(DataArrayPath, DataContainerName)
-	Q_PROPERTY(DataArrayPath DataContainerName READ getDataContainerName WRITE setDataContainerName)
+  SIMPL_FILTER_PARAMETER(QString, CellAttributeMatrixName)
+  Q_PROPERTY(QString CellAttributeMatrixName READ getCellAttributeMatrixName WRITE setCellAttributeMatrixName)
 
-	SIMPL_FILTER_PARAMETER(QString, CellAttributeMatrixName)
-	Q_PROPERTY(QString CellAttributeMatrixName READ getCellAttributeMatrixName WRITE setCellAttributeMatrixName)
+  SIMPL_FILTER_PARAMETER(QString, ImageDataArrayName)
+  Q_PROPERTY(QString ImageDataArrayName READ getImageDataArrayName WRITE setImageDataArrayName)
 
-	SIMPL_FILTER_PARAMETER(QString, ImageDataArrayName)
-	Q_PROPERTY(QString ImageDataArrayName READ getImageDataArrayName WRITE setImageDataArrayName)
+  SIMPL_FILTER_PARAMETER(int, OperationMode)
+  Q_PROPERTY(int OperationMode READ getOperationMode WRITE setOperationMode)
 
-	SIMPL_FILTER_PARAMETER(int, OperationMode)
-	Q_PROPERTY(int OperationMode READ getOperationMode WRITE setOperationMode)
+  SIMPL_FILTER_PARAMETER(FileListInfo_t, ImageFileListInfo)
+  Q_PROPERTY(FileListInfo_t ImageFileListInfo READ getImageFileListInfo WRITE setImageFileListInfo)
 
-	SIMPL_FILTER_PARAMETER(FileListInfo_t, ImageFileListInfo)
-	Q_PROPERTY(FileListInfo_t ImageFileListInfo READ getImageFileListInfo WRITE setImageFileListInfo)
-	
-	SIMPL_FILTER_PARAMETER(FileListInfo_t, OrientationFileListInfo)
-	Q_PROPERTY(FileListInfo_t OrientationFileListInfo READ getOrientationFileListInfo WRITE setOrientationFileListInfo)
+  SIMPL_FILTER_PARAMETER(FileListInfo_t, OrientationFileListInfo)
+  Q_PROPERTY(FileListInfo_t OrientationFileListInfo READ getOrientationFileListInfo WRITE setOrientationFileListInfo)
 
+  /**
+   * @brief getCompiledLibraryName Reimplemented from @see AbstractFilter class
+   */
+  const QString getCompiledLibraryName() const override;
 
-    /**
-     * @brief getCompiledLibraryName Reimplemented from @see AbstractFilter class
-     */
-    const QString getCompiledLibraryName() const override;
+  /**
+   * @brief getBrandingString Returns the branding string for the filter, which is a tag
+   * used to denote the filter's association with specific plugins
+   * @return Branding string
+   */
+  const QString getBrandingString() const override;
 
-    /**
-     * @brief getBrandingString Returns the branding string for the filter, which is a tag
-     * used to denote the filter's association with specific plugins
-     * @return Branding string
-    */
-    const QString getBrandingString() const override;
+  /**
+   * @brief getFilterVersion Returns a version string for this filter. Default
+   * value is an empty string.
+   * @return
+   */
+  const QString getFilterVersion() const override;
 
-    /**
-     * @brief getFilterVersion Returns a version string for this filter. Default
-     * value is an empty string.
-     * @return
-     */
-    const QString getFilterVersion() const override;
+  /**
+   * @brief newFilterInstance Reimplemented from @see AbstractFilter class
+   */
+  AbstractFilter::Pointer newFilterInstance(bool copyFilterParameters) const override;
 
-    /**
-     * @brief newFilterInstance Reimplemented from @see AbstractFilter class
-     */
-    AbstractFilter::Pointer newFilterInstance(bool copyFilterParameters) const override;
+  /**
+   * @brief getGroupName Reimplemented from @see AbstractFilter class
+   */
+  const QString getGroupName() const override;
 
-    /**
-     * @brief getGroupName Reimplemented from @see AbstractFilter class
-     */
-    const QString getGroupName() const override;
+  /**
+   * @brief getSubGroupName Reimplemented from @see AbstractFilter class
+   */
+  const QString getSubGroupName() const override;
 
-    /**
-     * @brief getSubGroupName Reimplemented from @see AbstractFilter class
-     */
-    const QString getSubGroupName() const override;
+  /**
+   * @brief getUuid Return the unique identifier for this filter.
+   * @return A QUuid object.
+   */
+  const QUuid getUuid() override;
 
-    /**
-     * @brief getUuid Return the unique identifier for this filter.
-     * @return A QUuid object.
-     */
-    const QUuid getUuid() override;
-  
-    /**
-     * @brief getHumanLabel Reimplemented from @see AbstractFilter class
-     */
-    const QString getHumanLabel() const override;
+  /**
+   * @brief getHumanLabel Reimplemented from @see AbstractFilter class
+   */
+  const QString getHumanLabel() const override;
 
-    /**
-     * @brief setupFilterParameters Reimplemented from @see AbstractFilter class
-     */
-    void setupFilterParameters() override;
+  /**
+   * @brief setupFilterParameters Reimplemented from @see AbstractFilter class
+   */
+  void setupFilterParameters() override;
 
-    /**
-     * @brief execute Reimplemented from @see AbstractFilter class
-     */
-    void execute() override;
+  /**
+   * @brief execute Reimplemented from @see AbstractFilter class
+   */
+  void execute() override;
 
-    /**
-    * @brief preflight Reimplemented from @see AbstractFilter class
-    */
-    void preflight() override;
+  /**
+   * @brief preflight Reimplemented from @see AbstractFilter class
+   */
+  void preflight() override;
 
-  signals:
-    /**
-     * @brief updateFilterParameters Emitted when the Filter requests all the latest Filter parameters
-     * be pushed from a user-facing control (such as a widget)
-     * @param filter Filter instance pointer 
-     */
-    void updateFilterParameters(AbstractFilter* filter);
+signals:
+  /**
+   * @brief updateFilterParameters Emitted when the Filter requests all the latest Filter parameters
+   * be pushed from a user-facing control (such as a widget)
+   * @param filter Filter instance pointer
+   */
+  void updateFilterParameters(AbstractFilter* filter);
 
-    /**
-     * @brief parametersChanged Emitted when any Filter parameter is changed internally
-     */
-    void parametersChanged();
+  /**
+   * @brief parametersChanged Emitted when any Filter parameter is changed internally
+   */
+  void parametersChanged();
 
-    /**
-     * @brief preflightAboutToExecute Emitted just before calling dataCheck()
-     */
-    void preflightAboutToExecute();
+  /**
+   * @brief preflightAboutToExecute Emitted just before calling dataCheck()
+   */
+  void preflightAboutToExecute();
 
-    /**
-     * @brief preflightExecuted Emitted just after calling dataCheck()
-     */
-    void preflightExecuted();
+  /**
+   * @brief preflightExecuted Emitted just after calling dataCheck()
+   */
+  void preflightExecuted();
 
-  protected:
-    ITKResampleImage();
+protected:
+  ITKResampleImage();
 
-    /**
-    * @brief dataCheck Checks for the appropriate parameter values and availability of arrays
-    */
-    void dataCheck();
+  /**
+   * @brief dataCheck Checks for the appropriate parameter values and availability of arrays
+   */
+  void dataCheck();
 
-    /**
-    * @brief Initializes all the private instance variables.
-    */
-    void initialize();
-  private:
-	  DEFINE_IDATAARRAY_VARIABLE(MovingImage)
-	  DEFINE_IDATAARRAY_VARIABLE(FinalImage)
+  /**
+   * @brief Initializes all the private instance variables.
+   */
+  void initialize();
 
-	  template <typename T> void Resample2D(QString sliceNo);
-	  ITKTransformHelpers getTransformAndFixedParams(QString sliceNo);
-	  template <typename T> void createCompatibleArrays();
-	  int32_t checkInputFileList(FileListInfo_t inputFileListInfo);
-	  QVector<QString> getFileList(FileListInfo_t inputFileListInfo);
-	  void SeriesResampling();
-	  void EBSDSeriesResampling();
-	  QString GetAndModifyHeader(QFile& reader, SizeVec3Type dims, FloatVec3Type spacing);
-	  void WriteResampledCTFfile(QString filename, std::vector<IDataArray::Pointer> ctfArrays, SizeVec3Type dims, FloatVec3Type spacing, size_t index);
-	  QVector<QString> m_ctfDataArrayNames;
-	  
-  public:
-    /* Rule of 5: All special member functions should be defined if any are defined.
-    * https://github.com/isocpp/CppCoreGuidelines/blob/master/CppCoreGuidelines.md#c21-if-you-define-or-delete-any-default-operation-define-or-delete-them-all
-    */
-    ITKResampleImage(const ITKResampleImage&) = delete;             // Copy Constructor Not Implemented
-    ITKResampleImage& operator=(const ITKResampleImage&) = delete;  // Copy Assignment Not Implemented
-    ITKResampleImage(ITKResampleImage&&) = delete;                  // Move Constructor Not Implemented
-    ITKResampleImage& operator=(ITKResampleImage&&) = delete;       // Move Assignment Not Implemented
+private:
+  DEFINE_IDATAARRAY_VARIABLE(MovingImage)
+  DEFINE_IDATAARRAY_VARIABLE(FinalImage)
 
+  template <typename T>
+  void Resample2D(const QString& sliceNo);
+  ITKTransformHelpers getTransformAndFixedParams(QString sliceNo);
+
+  template <typename T>
+  void createCompatibleArrays();
+
+  int32_t checkInputFileList(FileListInfo_t inputFileListInfo);
+
+  QVector<QString> getFileList(FileListInfo_t inputFileListInfo);
+
+  void SeriesResampling();
+
+  void EBSDSeriesResampling();
+
+  QString GetAndModifyHeader(QFile& reader, SizeVec3Type dims, FloatVec3Type spacing);
+
+  void WriteResampledCTFfile(const QString& filename, std::vector<IDataArray::Pointer> ctfArrays, SizeVec3Type dims, FloatVec3Type spacing, size_t index);
+
+  QVector<QString> m_ctfDataArrayNames;
+
+public:
+  ITKResampleImage(const ITKResampleImage&) = delete;            // Copy Constructor Not Implemented
+  ITKResampleImage& operator=(const ITKResampleImage&) = delete; // Copy Assignment Not Implemented
+  ITKResampleImage(ITKResampleImage&&) = delete;                 // Move Constructor Not Implemented
+  ITKResampleImage& operator=(ITKResampleImage&&) = delete;      // Move Assignment Not Implemented
 };
-
--- DREAM3DReviewFilters/util/EBSDWriterFactory.cpp
+++ DREAM3DReviewFilters/util/EBSDWriterFactory.cpp
@@ -1,75 +1,80 @@
 #include "EBSDWriterFactory.h"
 
-namespace EBSDWriterHelper 
+#include <cmath>
+#include <fstream>
+
+#include "SIMPLib/DataArrays/DataArray.hpp"
+#include "SIMPLib/DataArrays/StringDataArray.h"
+
+namespace EBSDWriterHelper
 {
-	template <typename T> inline std::function<QString(QString, IDataArray::Pointer, uint64_t)> ArrayWriterFactory()
-	{
-		return WriteArrayToQStringLine<T>;
-	}
 
-	template <typename T> inline QString WriteArrayToQStringLine(QString rowData, IDataArray::Pointer ptr, uint64_t index)
-	{
-		typename DataArray<T>::Pointer data = std::dynamic_pointer_cast<DataArray<T>>(ptr);
-		int numComps = data->getNumberOfComponents();
+template <typename T>
+inline QString WriteArrayToQStringLine(QString rowData, const IDataArray::Pointer& ptr, uint64_t index)
+{
+  typename DataArray<T>::Pointer data = std::dynamic_pointer_cast<DataArray<T>>(ptr);
+  int numComps = data->getNumberOfComponents();
 
+  for(int i = 0; i < numComps; i++)
+  {
+    T value = data->getValue(numComps * index + i);
+    QString arrayElement = QString::number(value);
+    rowData.append(arrayElement);
+    if(i != (numComps - 1))
+    {
+      rowData.append("	");
+    }
+  }
 
-		for (int i = 0; i < numComps; i++)
-		{
-			typename T value = data->getValue(numComps*index + i);
-			QString arrayElement = QString::number(value);
-			rowData.append(arrayElement);
-			if (i != (numComps - 1))
-			{
-				rowData.append("	");
-			}
-		}
-
-
-		return rowData;
-	}
-
-	template <> inline QString WriteArrayToQStringLine<int32_t>(QString rowData, IDataArray::Pointer ptr, uint64_t index)
-	{
-		DataArray<int32_t>::Pointer data = std::dynamic_pointer_cast<DataArray<int32_t>>(ptr);
-		int numComps = data->getNumberOfComponents();
-
-
-		for (int i = 0; i < numComps; i++)
-		{
-			int32_t value = data->getValue(numComps*index + i);
-			QString arrayElement = QString::number(value);
-			rowData.append(arrayElement);
-			if (i != (numComps - 1))
-			{
-				rowData.append("	");
-			}
-		}
-
-	
-		return rowData;
-	}
-
-	template <> inline QString WriteArrayToQStringLine<float>(QString rowData, IDataArray::Pointer ptr, uint64_t index)
-	{
-		DataArray<float>::Pointer data = std::dynamic_pointer_cast<DataArray<float>>(ptr);
-		int numComps = data->getNumberOfComponents();
-
-
-		for (int i = 0; i < numComps; i++)
-		{
-			float value = data->getValue(numComps*index + i);
-			QString arrayElement = QString::number(value, 'f', 4);
-			rowData.append(arrayElement);
-			if (i != (numComps - 1))
-			{
-				rowData.append("	");
-			}
-		}
-
-		return rowData;
-	}
-
+  return rowData;
 }
+
+template <>
+inline QString WriteArrayToQStringLine<int32_t>(QString rowData, const IDataArray::Pointer& ptr, uint64_t index)
+{
+  DataArray<int32_t>::Pointer data = std::dynamic_pointer_cast<DataArray<int32_t>>(ptr);
+  int numComps = data->getNumberOfComponents();
+
+  for(int i = 0; i < numComps; i++)
+  {
+    int32_t value = data->getValue(numComps * index + i);
+    QString arrayElement = QString::number(value);
+    rowData.append(arrayElement);
+    if(i != (numComps - 1))
+    {
+      rowData.append("	");
+    }
+  }
+
+  return rowData;
+}
+
+template <>
+inline QString WriteArrayToQStringLine<float>(QString rowData, const IDataArray::Pointer& ptr, uint64_t index)
+{
+  DataArray<float>::Pointer data = std::dynamic_pointer_cast<DataArray<float>>(ptr);
+  int numComps = data->getNumberOfComponents();
+
+  for(int i = 0; i < numComps; i++)
+  {
+    float value = data->getValue(numComps * index + i);
+    QString arrayElement = QString::number(value, 'f', 4);
+    rowData.append(arrayElement);
+    if(i != (numComps - 1))
+    {
+      rowData.append("	");
+    }
+  }
+
+  return rowData;
+}
+
+template <typename T>
+inline std::function<QString(QString, IDataArray::Pointer, uint64_t)> ArrayWriterFactory()
+{
+  return WriteArrayToQStringLine<T>;
+}
+} // namespace EBSDWriterHelper
 
 EBSDWriterFactory* EBSDWriterFactory::self = nullptr;
 
@@ -85,9 +90,7 @@ EBSDWriterFactory::EBSDWriterFactory()
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
-EBSDWriterFactory::~EBSDWriterFactory()
-{
-}
+EBSDWriterFactory::~EBSDWriterFactory() = default;
 
 // -----------------------------------------------------------------------------
 //
@@ -104,20 +107,19 @@ EBSDWriterFactory* EBSDWriterFactory::Instance()
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
-std::function<QString(QString, IDataArray::Pointer, uint64_t)> EBSDWriterFactory::getWriter(QString name)
+std::function<QString(QString, const IDataArray::Pointer&, uint64_t)> EBSDWriterFactory::getWriter(const QString& name)
 {
-	std::function<QString(QString, IDataArray::Pointer, uint64_t)> writer = nullptr;
-	try
-	{
-		writer = m_DataTypes.at(name);
-	}
-	catch (const std::out_of_range&)
-	{
-		//std::string info("Data type index from file: " + std::to_string(index) + "\n" + "Supported data type indices: \n" + getListOfSupportedDataTypes());
-		//throw FatalTDMSException(TDMSExceptionMessages::UnsupportedDataType, info);
-	}
+  std::function<QString(QString, IDataArray::Pointer, uint64_t)> writer = nullptr;
+  try
+  {
+    writer = m_DataTypes.at(name);
+  } catch(const std::out_of_range&)
+  {
+    // std::string info("Data type index from file: " + std::to_string(index) + "\n" + "Supported data type indices: \n" + getListOfSupportedDataTypes());
+    // throw FatalTDMSException(TDMSExceptionMessages::UnsupportedDataType, info);
+  }
 
-	return writer;
+  return writer;
 }
 
 // -----------------------------------------------------------------------------
@@ -125,15 +127,15 @@ std::function<QString(QString, IDataArray::Pointer, uint64_t)> EBSDWriterFactory
 // -----------------------------------------------------------------------------
 void EBSDWriterFactory::initializeDataTypes()
 {
-	m_DataTypes["Phases"] = EBSDWriterHelper::ArrayWriterFactory<int>();
-	m_DataTypes["Error"] = EBSDWriterHelper::ArrayWriterFactory<int>();
-	m_DataTypes["MAD"] = EBSDWriterHelper::ArrayWriterFactory<float>();
-	m_DataTypes["BC"] = EBSDWriterHelper::ArrayWriterFactory<int>();
-	m_DataTypes["EulerAngles"] = EBSDWriterHelper::ArrayWriterFactory<float>();
-	m_DataTypes["Bands"] = EBSDWriterHelper::ArrayWriterFactory<int>();
-	m_DataTypes["BS"] = EBSDWriterHelper::ArrayWriterFactory<int>();
-	m_DataTypes["X"] = EBSDWriterHelper::ArrayWriterFactory<float>();
-	m_DataTypes["Y"] = EBSDWriterHelper::ArrayWriterFactory<float>();
+  m_DataTypes["Phases"] = EBSDWriterHelper::ArrayWriterFactory<int>();
+  m_DataTypes["Error"] = EBSDWriterHelper::ArrayWriterFactory<int>();
+  m_DataTypes["MAD"] = EBSDWriterHelper::ArrayWriterFactory<float>();
+  m_DataTypes["BC"] = EBSDWriterHelper::ArrayWriterFactory<int>();
+  m_DataTypes["EulerAngles"] = EBSDWriterHelper::ArrayWriterFactory<float>();
+  m_DataTypes["Bands"] = EBSDWriterHelper::ArrayWriterFactory<int>();
+  m_DataTypes["BS"] = EBSDWriterHelper::ArrayWriterFactory<int>();
+  m_DataTypes["X"] = EBSDWriterHelper::ArrayWriterFactory<float>();
+  m_DataTypes["Y"] = EBSDWriterHelper::ArrayWriterFactory<float>();
 }
 
 // -----------------------------------------------------------------------------
